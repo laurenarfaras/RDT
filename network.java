63972bebb5b4d6bb0470b1c89fb0b74a610267b1
Lauren Arfaras <laurenarfaras@Laurens-MacBook-Pro.local>
2018-03-16 16:31:07 -0400

    initial commit

---
+++ network.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.net.*;
+
+public class network {
+
+  public static void main(String[] args) {
+
+    // check for port number
+    int port = -1;
+    if (args.length == 1) {
+      port = Integer.parseInt(args[0]);
+    } else {
+      System.out.println("Please enter a port number.");
+      System.exit(0);
+    }
+
+    // create server socket instance
+    ServerSocket serverSocket = new ServerSocket(port);
+
+  }
+
+}
