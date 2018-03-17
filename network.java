5a7088e9558fbbdc19922fa57afb540a58b355ea
Lauren Arfaras <laurenarfaras@wsip-68-110-129-180.ga.at.cox.net>
2018-03-17 17:01:22 -0400

    creates random number to decide what operation to choose

--- network.java
+++ network.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.util.*;
 import java.net.*;
+import java.lang.*;
 
 public class network {
 
@@ -15,14 +16,53 @@ public class network {
       System.exit(0);
     }
 
-    // create server socket instance
-    ServerSocket serverSocket = new ServerSocket(port);
+    try {
+      // create server socket instance
+      ServerSocket serverSocket = new ServerSocket(port);
 
-    listen:
-    System.out.println("listening...");
-    serverSocket.accept();
+      // listen for the two incoming connections
+      listen:
+      System.out.println("listening...");
+      serverSocket.accept();
+      serverSocket.accept();
+      System.out.println("Two sockets connected");
 
+    } catch (Exception e) {
+      System.out.println("Error: " + e);
+    }
 
+    mainmenu();
+
+  }
+
+  public static void mainmenu(){
+
+    Scanner sc = new Scanner(System.in);
+    String command = sc.nextLine();
+    System.out.println("command: " + command);
+
+    if (command.equals("bye")) {
+      System.exit(0);
+    }
+
+    // choose a random value to determine operation
+    double r = Math.random();
+    System.out.println("random number: " + r);
+
+    boolean pass, corrupt, drop = false;
+
+    if (r <= 0.50) {
+      pass = true;
+      System.out.println("pass");
+    } else if (r > 0.50 && r  <= 0.75) {
+      corrupt = true;
+      System.out.println("corrupt");
+    } else if (r > 0.75) {
+      drop = true;
+      System.out.println("drop");
+    }
+
+    mainmenu();
 
   }
 
