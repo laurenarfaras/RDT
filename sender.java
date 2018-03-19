49b047c62d62ec87e73d1243bb55d5f056370cbb
Lauren Arfaras <laurenarfaras@64-238-186-181.cty.apt.gru.net>
2018-03-19 09:45:41 -0400

    created a different loop for sender

--- sender.java
+++ sender.java
@@ -1,16 +1,14 @@
 import java.io.*;
-import java.util.*;
+import java.util.Scanner;
 import java.net.*;
-import java.lang.*;
 import java.nio.file.*;
 
 public class sender {
 
   public static void main(String[] args) throws IOException {
 
-    // check for url, port, and message file name
     String url = "";
-    int port = -1;
+    int port = 0;
     String msgFileName = "";
     if (args.length == 3) {
       url = args[0].replaceAll("\\s+", "");
@@ -29,21 +27,32 @@ public class sender {
       System.out.println(fileSplit[i]);
     }
 
-    // start new socket connection with the url and port
-    Socket s = new Socket(url, port);
-    // create reading/writing streams to communicate with the network socket server
-    PrintStream networkOut = new PrintStream(s.getOutputStream());
-    BufferedReader networkIn = new BufferedReader(new InputStreamReader(s.getInputStream()));
-    // call mainmenu function to begin communicating with network
-    mainmenu(s, networkOut, networkIn, fileSplit);
+    // create socket
+    try {
+      Socket s = new Socket(url, port);
+      System.out.println(s);
+      // Scanner to read in from server side
+      Scanner networkIn = new Scanner(new InputStreamReader(s.getInputStream()));
+      // PrintStream to send to server
+      PrintStream networkOut = new PrintStream(s.getOutputStream());
 
-  }
+      String messageIn = networkIn.nextLine();
+      System.out.println("message in: " + messageIn);
 
-  public static void mainmenu(Socket s, PrintStream networkOut, BufferedReader networkIn, String[] fileSplit) {
+      // create loop to handle network communication until termination
+      while (true) {
+        // send output to server
+        String messageOut = "0 1 317 You.";
+        networkOut.println(messageOut);
+        // get feedback ACK from network
+        messageIn = networkIn.nextLine();
+        System.out.println("message in: " + messageIn);
+      }
 
-    
-
-    // mainmenu(s, networkOut, networkIn, fileSplit);
+    } catch(IOException err) {
+      System.out.println("Socket could not be created.");
+      System.exit(0);
+    }
 
   }
 
