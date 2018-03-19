d76375f24b96768e47274216e4d966737a8cb9cd
Lauren Arfaras <laurenarfaras@64-238-186-181.cty.apt.gru.net>
2018-03-19 09:57:01 -0400

    sends entire file

--- sender.java
+++ sender.java
@@ -5,6 +5,9 @@ import java.nio.file.*;
 
 public class sender {
 
+  public static String seqNo = "0";
+  public static int id = 1;
+
   public static void main(String[] args) throws IOException {
 
     String url = "";
@@ -41,12 +44,29 @@ public class sender {
 
       // create loop to handle network communication until termination
       while (true) {
+        boolean verified = false;
         // send output to server
-        String messageOut = "0 1 317 You.";
+        // String messageOut = "0 1 317 You";
+        String checksum = "100";
+        String messageOut = seqNo + " " + Integer.toString(id) + " " + checksum + " " + fileSplit[id-1];
+        System.out.println("message out: " + messageOut);
         networkOut.println(messageOut);
         // get feedback ACK from network
         messageIn = networkIn.nextLine();
         System.out.println("message in: " + messageIn);
+        String[] inputSplit = messageIn.split("\\s+");
+        if (inputSplit[0].equals(seqNo)) {
+          verified = true;
+        }
+        System.out.println("verified? " + verified);
+        if (verified) {
+          id = id + 1;
+          if (seqNo.equals("0")) {
+            seqNo = "1";
+          } else {
+            seqNo = "0";
+          }
+        }
       }
 
     } catch(IOException err) {
